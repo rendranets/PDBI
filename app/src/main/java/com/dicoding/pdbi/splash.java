e412f994547e448b678084817ed52e09a6198eab
rendranets <39667193+rendranets@users.noreply.github.com>
2020-02-13 19:41:31 +0700

    PDBI

---
+++ app/src/main/java/com/dicoding/pdbi/splash.java
@@ -0,0 +1,41 @@
+package com.dicoding.pdbi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash extends AppCompatActivity {
+
+    private ImageView iv_splash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        iv_splash = (ImageView) findViewById(R.id.iv_splash);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.transition);
+
+        iv_splash.startAnimation(myanim);
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread(){
+            public void run () {
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+                timer.start();
+    }
+}
